cc3ed4fba6c94bcb6685a9a3ee5fe393ba952102
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-06-27 23:11:04 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -5,9 +5,9 @@
 int fdef[]={190,500,3000,6000,15000};
 int fdef_size=5;
 
-float def_attack[]={0.0000001,0.0000002,0.00000001,0.0000001,0.00001};
-float def_release[]={0.00000000000001,0.00000000001,0.000000000001,0.0000000000000001,0.00000001};
-float def_target[]={9000,17000,17000,18000,21000};
+float def_attack[]={0.000000000001,0.000000000001,0.00000001,0.0000001,0.00001};
+float def_release[]={0.000000000000001,0.0000000000001,0.000000000001,0.0000000000000001,0.00000001};
+float def_target[]={9000,18000,18000,19000,22000};
 float def_m_gain[]={400,400,400,400,400};
 float pre_amp[]={0.5,0.7,1,4,18};
 float def_gate[]={6000,3000,4000,4000,4000};
@@ -15,8 +15,8 @@ int bypass[]={0,0,0,0,0};
 float post_amp[]={1,1,1,1,3};
 int types[]={COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS};
 
-#define FINAL_AMP 1
-//#define FINAL_CLIP
+#define FINAL_AMP 0.7
+#define FINAL_CLIP
 
 
 #define RECORDING_IFACE "default"
@@ -29,7 +29,7 @@ int types[]={COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS};
 
 #define AGC_TARG 15000
 
-#define AGC_SPEED 0.00001
+#define AGC_SPEED 0.000001
 #define AGC_GATE 5000
 
 // set to 1 to show the levels in real time, 0 to keep silent
